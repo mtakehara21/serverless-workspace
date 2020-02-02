8f0b90c4dc2f9151a20b0ab2a0103f0adcc2e8c7
Masahiro Takehara <azukibar21@gmail.com>
2020-02-02 23:05:15 +0900

    shop recommend

--- handler.py
+++ handler.py
@@ -1,24 +1,30 @@
 import json
+import urllib.request
+import os
+import xml.etree.ElementTree as ET
 
+def post_slack(event, context):
 
-def main(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+    url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=f096f90662cd839e&lat=35.726&lng=139.66&range=5&order=4&count=1'
+    
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        message = response.read()
+        
+    message = ET.fromstring(message)      
+    message = "お家の近くのおすすめのお店 \n" + message[4][1].text + '\n' + message[4][4].text + '\n' + message[4][22][0].text
+
+    send_data = {
+        "username": "近所のオススメのお店",
+        "icon_emoji": ":beer:",
+        "text": message,
     }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    
+    send_text = "payload=" + json.dumps(send_data)
+    request = urllib.request.Request(
+        os.environ['slack_api'],
+        data=send_text.encode("utf-8"), 
+        method="POST"
+    )
+    with urllib.request.urlopen(request) as response:
+        response_body = response.read().decode("utf-8")    
